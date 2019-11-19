deb234f6323d9d021e3ee75e8e5484b902309f82
unknown <2133832@labinfo.is.escuelaing.edu.co>
2019-11-19 18:40:29 -0500

    Hacer Fila Prueba

--- src/main/resources/static/js/colaApp.js
+++ src/main/resources/static/js/colaApp.js
@@ -11,7 +11,7 @@ var colaApp =( function (){
    
     var añadirAlaCola=function(tiquetes){
         var tiqDisponibles=tiquetes.length;
-        var atraccion = sessionStorage.getItem('Objetoatraccion');
+        var atraccion = sessionStorage.getItem('atraccion');
         var cantidadAIngresar = $('#cantidad').val();
         if (cantidadAIngresar>tiqDisponibles){
             alert("Cantidad de tiquetes ingresados no es suficiente para que esa cantidad de personas hagan fila");
@@ -19,7 +19,8 @@ var colaApp =( function (){
         }
         else{
             for (var i=0;i<cantidadAIngresar;i++){
-                var cola={"atraccion":atraccion,"tiquete":tiquetes[i]}
+                var cola={"atraccion":atraccion,"tiquete":tiquetes[i].id}
+                alert(cola);
                 colaClient.saveCola(cola);
             }
 
