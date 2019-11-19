11f6a674b0175a9d03de701b339e4dc9c8d0937f
unknown <2133832@labinfo.is.escuelaing.edu.co>
2019-11-19 14:27:05 -0500

    GetCola Corregido

--- src/main/java/edu/eci/arsw/chillpark/repository/impl/ColaRepositoryImpl.java
+++ src/main/java/edu/eci/arsw/chillpark/repository/impl/ColaRepositoryImpl.java
@@ -24,7 +24,7 @@ public class ColaRepositoryImpl implements ColaRepositoryCustom {
 
     @Override
     public List<Tiquete> findByAtraccion(int atraccion) {
-        Query query = entityManager.createQuery("select t from Atraccionusuario au , Tiquete t where au.atraccion=:idAtraccion and t.id=au.tiquete", Tiquete.class);
+        Query query = entityManager.createQuery("select t from Atraccionusuario au , tiquete t where au.atraccion=:idAtraccion and t.id=au.tiquete", Tiquete.class);
         
         query.setParameter("idAtraccion", atraccion);
         return query.getResultList();
@@ -32,7 +32,7 @@ public class ColaRepositoryImpl implements ColaRepositoryCustom {
 
     @Override
     public List<Atraccion> findByUsuario(int tiquete) {
-        Query query = entityManager.createQuery("select a from Atraccionusuario au ,Atraccion a where tiquete=:tiqid and au.atraccion=a.id", Atraccion.class);
+        Query query = entityManager.createQuery("select a from Atraccionusuario au , Atraccion a where au.tiquete=:tiqid and a.id=au.atraccion", Atraccion.class);
         
         query.setParameter("tiqid", tiquete);
         return query.getResultList();
