4d510e230b0dcdde0971942f0063a7fd58ab45e0
unknown <2133832@labinfo.is.escuelaing.edu.co>
2019-11-19 17:45:51 -0500

    Get COlasByUser Prueba

--- src/main/resources/static/js/atraccionApp.js
+++ src/main/resources/static/js/atraccionApp.js
@@ -136,7 +136,7 @@ var atraccionApp =( function (){
                         if(atraccion.activo){
                             var activo="<span style=\"color:green;font-weight:bold\"> Abierta </span></br>";
              
-                            boton = " <a href=\"javascript:atraccionApp.hacerFila("+atraccion+")\" class=\"button\">"+tipo+"</a></div>"; 
+                            boton = " <a href=\"javascript:atraccionApp.hacerFila("+JSON.stringify(atraccion)+")\" class=\"button\">"+tipo+"</a></div>"; 
                         }
                         else{
                             var activo="<span style=\"color:red;font-weight:bold\">Cerrada</span></br>";
