b418c3c0606b28096c4dc99d9c0fee048156d21a
unknown <2133832@labinfo.is.escuelaing.edu.co>
2019-11-25 12:27:29 -0500

    Hacer fila prueba

--- src/main/resources/static/js/colaApp.js
+++ src/main/resources/static/js/colaApp.js
@@ -16,8 +16,6 @@ var colaApp =( function (){
         var atraccion = sessionStorage.getItem('atraccion');
         var cantidadAIngresar = $('#cantidad').val();
         var cantidadDisponible = numTiquetes - tiquetes.length;
-        alert("Empieza en "+(numTiquetes-cantidadDisponible));
-        alert ("Termina en "+ ((numTiquetes-cantidadDisponible)+(cantidadAIngresar-1)) );
         if (cantidadAIngresar>0){
             if (cantidadAIngresar>numTiquetes){
                 alert("Solo tiene disponibles por el momento"+ cantidadDisponible +" espacios para la fila");
@@ -27,7 +25,8 @@ var colaApp =( function (){
                 alert("Ya hay "+tiquetes.length+" personas haciendo fila, solo tiene disponibles "+ cantidadDisponible +" espacios para la fila");
             }
             else{
-                for (var i=numTiquetes-cantidadDisponible;i<(numTiquetes-cantidadDisponible)+(cantidadAIngresar-1);i++){
+                for (var i=numTiquetes-cantidadDisponible;i<=(numTiquetes-cantidadDisponible)+(cantidadAIngresar-1);i++){
+                    alert(i);
                     var cola={"atraccion":parseInt(atraccion),"tiquete":tiquetes[i].id};
                     
                     colaClient.saveCola(cola);
